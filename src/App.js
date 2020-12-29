26d2ef1445b8bf3ccfd7f72e3455b49fb76299fd
Steve B <sbogucki@mail.usf.edu>
2020-12-29 15:00:00 -0800

    created under construction page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src="/images/dwalesska_smalllogo.jpg" className="App-logo" alt="logo" />
+				<p>👋 Site currently under construction.</p>
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+					<div>
+						<img src="/images/ig_icon.svg" className="igIcon" alt="instagram" />
+					</div>
+					<div>
+						{' '}
+						<a
+							className="App-link"
+							href="https://www.instagram.com/dwalesska_coaching/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							@dwalesska_coaching
+						</a>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
