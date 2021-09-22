cd823661b44495b3969d0d93cac0778ee3b4470b
Steve B <sbogucki@mail.usf.edu>
2021-09-21 20:28:17 -0700

    added language slider to header

--- src/Desktop/Header/HeaderDesktop.jsx
+++ src/Desktop/Header/HeaderDesktop.jsx
@@ -1,12 +1,26 @@
 import "./headerDesktop.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const HeaderDesktop = () => {
   return (
     <div className="headerDesktopContainer">
-      <Link to="/" className="headerLink"><span className="headerLink">HOME</span></Link>
-      <Link to="/about" className="headerLink"><span>ABOUT US</span></Link>
-      <span className="headerLink">CONTACT</span>
+      <div className="headerLinkContainer">
+        <Link to="/" className="headerLink">
+          <span className="headerLink">HOME</span>
+        </Link>
+        <Link to="/about" className="headerLink">
+          <span>ABOUT US</span>
+        </Link>
+        <span className="headerLink">CONTACT</span>
+      </div>
+      <div className="headerLanguageSliderContainer">
+        <span>English</span>{" "}
+        <label className="headerLanguageSwitch">
+          <input type="checkbox" />
+          <span className="headerLanguageSlider"></span>
+        </label>{" "}
+        <span>Spanish</span>
+      </div>
     </div>
   );
 };
